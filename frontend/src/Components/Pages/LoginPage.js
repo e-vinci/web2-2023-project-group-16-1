214c82c5db76e7562cf0283f31dc01eec0c13e40
Mykhailo Kretsu <mykhailo.kretsu@student.vinci.be>
2023-11-07 15:05:32 +0100

    added auths to frontend

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -1,3 +1,8 @@
+import { setAuthenticatedUser } from "../../utils/auths";
+import Navbar from '../Navbar/Navbar';
+import Navigate from '../Router/Navigate';
+
+
 const LoginPage = () => {
     const login = `<div class="hero min-h-screen bg-base-200">
     <div class="hero-content flex-col lg:flex-row-reverse">
@@ -11,19 +16,19 @@ const LoginPage = () => {
             <label class="label">
               <span class="label-text">Email</span>
             </label>
-            <input type="email" placeholder="email" class="input input-bordered" required />
+            <input id="email" type="email" placeholder="email" class="input input-bordered" required />
           </div>
           <div class="form-control">
             <label class="label">
               <span class="label-text">Password</span>
             </label>
-            <input type="password" placeholder="password" class="input input-bordered" required />
+            <input id="password" type="password" placeholder="password" class="input input-bordered" required />
             <label class="label">
               <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
             </label>
           </div>
           <div class="form-control mt-6">
-            <button class="btn btn-primary">Login</button>
+            <button class="btn btn-primary" id="loginbtn">Login</button>
           </div>
         </form>
       </div>
@@ -31,7 +36,53 @@ const LoginPage = () => {
   </div>`
     const main = document.querySelector('main');
     main.innerHTML = login;
-  };
+
+    const btn = document.getElementById("loginbtn");
+    btn.addEventListener("click", async(e) =>{
+      e.preventdefault();
+      
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      const newData={
+        email,
+        password
+      }
+
+      try {
+        const options = {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          body: JSON.stringify(newData),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+
+        const reponse = await fetch(`/api/auths/login`, options);
+        if (!reponse.ok) {
+                    throw new Error(
+            // eslint-disable-next-line no-irregular-whitespace
+            `fetch error : ${reponse.status} : ${reponse.statusText}`,
+          );
+        }
+
+        const user = await reponse.json();
+
+        // sets the Authenticated user to the actual user
+        await setAuthenticatedUser(user);
+
+        // reloads Navbar (display is different when user logged in)
+        await Navbar();
+
+        // navigte to homePage
+        // clearActive();
+        await Navigate('/');
+      } catch (err) {
+        // eslint-disable-next-line
+        console.error('error: ', err);
+      }
+    });
+  }
   
   export default LoginPage;
   
---
+++ frontend/src/utils/auths.js
@@ -0,0 +1,26 @@
+let currentUser;
+
+// eslint-disable-next-line consistent-return
+const getAuthenticatedUser = () => {
+  if (currentUser !== undefined) return currentUser;
+  const serializedUser = localStorage.getItem("user");
+  if (!serializedUser) return undefined;
+  currentUser = JSON.parse(serializedUser);
+}
+// défini l'utilisateur authentifié actuellement 
+const setAuthenticatedUser = (authenticatedUser, ) => {
+  const serializedUser = JSON.stringify(authenticatedUser);
+  localStorage.setItem("user", serializedUser);
+  currentUser = authenticatedUser;
+};
+
+// indique si l'utiliseur actuel est authentifié ou pas 
+const isAuthenticated = () => currentUser !== undefined;
+
+// retire l'utilisateur authentifié
+const clearAuthenticatedUser = () => {
+  localStorage.removeItem("user");
+  currentUser = undefined;
+};
+
+export { getAuthenticatedUser, setAuthenticatedUser, isAuthenticated, clearAuthenticatedUser };
