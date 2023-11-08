35678a420a19fccf826d07a80150bc27dd19fdf5
Steins <104737710+Steinzu@users.noreply.github.com>
2023-11-08 20:37:29 +0100

    Modified the backend so that it uses our pocketbase db

--- api/models/users.js
+++ api/models/users.js
@@ -1,101 +1,26 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const path = require('node:path');
-const { parse, serialize } = require('../utils/json');
+/* eslint-disable quote-props */
+// eslint-disable-next-line import/no-unresolved
+const PocketBase = require('pocketbase/cjs');
 
-const jwtSecret = 'ilovemypizza!';
-const lifetimeJwt = 24 * 60 * 60 * 1000; // in ms : 24 * 60 * 60 * 1000 = 24h
+const pb = new PocketBase('https://pocketbasehop.hop.sh');
 
-const saltRounds = 10;
-
-const jsonDbPath = path.join(__dirname, '/../data/users.json');
-
-const defaultUsers = [
-  {
-    id: 1,
-    username: 'admin',
-    password: bcrypt.hashSync('admin', saltRounds),
-  },
-];
-
-async function login(username, password) {
-  const userFound = readOneUserFromUsername(username);
-  if (!userFound) return undefined;
-
-  const passwordMatch = await bcrypt.compare(password, userFound.password);
-  if (!passwordMatch) return undefined;
-
-  const token = jwt.sign(
-    { username }, // session data added to the payload (payload : part 2 of a JWT)
-    jwtSecret, // secret used for the signature (signature part 3 of a JWT)
-    { expiresIn: lifetimeJwt }, // lifetime of the JWT (added to the JWT payload)
-  );
-
-  const authenticatedUser = {
-    username,
-    token,
+async function register(username, email, password, passwordConfirm) {
+  const data = {
+    'username': username,
+    'email': email,
+    'password': password,
+    'passwordConfirm': passwordConfirm,
   };
-
-  return authenticatedUser;
+  const record = await pb.collection('users').create(data);
+  return record;
 }
 
-async function register(username, password) {
-  const userFound = readOneUserFromUsername(username);
-  if (userFound) return undefined;
-
-  await createOneUser(username, password);
-
-  const token = jwt.sign(
-    { username }, // session data added to the payload (payload : part 2 of a JWT)
-    jwtSecret, // secret used for the signature (signature part 3 of a JWT)
-    { expiresIn: lifetimeJwt }, // lifetime of the JWT (added to the JWT payload)
-  );
-
-  const authenticatedUser = {
-    username,
-    token,
-  };
-
-  return authenticatedUser;
-}
-
-function readOneUserFromUsername(username) {
-  const users = parse(jsonDbPath, defaultUsers);
-  const indexOfUserFound = users.findIndex((user) => user.username === username);
-  if (indexOfUserFound < 0) return undefined;
-
-  return users[indexOfUserFound];
-}
-
-async function createOneUser(username, password) {
-  const users = parse(jsonDbPath, defaultUsers);
-
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  const createdUser = {
-    id: getNextId(),
-    username,
-    password: hashedPassword,
-  };
-
-  users.push(createdUser);
-
-  serialize(jsonDbPath, users);
-
-  return createdUser;
-}
-
-function getNextId() {
-  const users = parse(jsonDbPath, defaultUsers);
-  const lastItemIndex = users?.length !== 0 ? users.length - 1 : undefined;
-  if (lastItemIndex === undefined) return 1;
-  const lastId = users[lastItemIndex]?.id;
-  const nextId = lastId + 1;
-  return nextId;
+async function login(email, password) {
+  const authData = await pb.collection('users').authWithPassword(email, password);
+  return authData;
 }
 
 module.exports = {
-  login,
   register,
-  readOneUserFromUsername,
+  login,
 };
