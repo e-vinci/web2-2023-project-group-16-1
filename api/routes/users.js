3047eb39c1f7409f21bac0ea599323b2a429bc67
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-11-13 11:39:10 +0100

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 const express = require('express');
-const { subscribe } = require('../models/users');
+const { subscribe, unSubscribe } = require('../models/users');
 
 const router = express.Router();
 
@@ -10,6 +10,18 @@ router.post('/subscribe', async (req, res) => {
   if (!url) return res.sendStatus(400); // 400 Bad Request
 
   const subscription = await subscribe(url);
+
+  if (!subscription) return res.sendStatus(500);
+
+  return res.json(subscription);
+});
+
+router.post('/unsubscribe', async (req, res) => {
+  const url = req?.body?.url?.length !== 0 ? req.body.url : undefined;
+
+  if (!url) return res.sendStatus(400); // 400 Bad Request
+
+  const subscription = await unSubscribe(url);
 
   if (!subscription) return res.sendStatus(500);
 
