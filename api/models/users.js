35796d9c2e4fcf3ae471123fff73af24650cb58d
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-11-13 11:39:12 +0100

    Update users.js

--- api/models/users.js
+++ api/models/users.js
@@ -6,7 +6,6 @@ const pb = new PocketBase('https://socialsync.hop.sh');
 
 const { getCurrentUser } = require('./auths');
 
-// eslint-disable-next-line no-unused-vars
 async function subscribe(url) {
   const urlObject = await pb.collection('urls').getFullList({
     filter: `url = "${url}"`,
@@ -29,6 +28,36 @@ async function subscribe(url) {
   return record;
 }
 
+async function unSubscribe(url) {
+  const urlObject = await pb.collection('urls').getFullList({
+    filter: `url = "${url}"`,
+  });
+
+  const currentUser = await getCurrentUser();
+
+  const data = {
+    user: currentUser.record.id,
+    url: urlObject[0].id,
+  };
+
+  const subscriptionObject = await pb.collection('subscriptions').getFullList({
+    filter: `url = "${data.url}" && user = "${data.user}"`,
+  });
+
+  let record;
+
+  try {
+    record = await pb.collection('subscriptions').delete(
+      subscriptionObject[0].id,
+    );
+  } catch (error) {
+    return error;
+  }
+
+  return record;
+}
+
 module.exports = {
   subscribe,
+  unSubscribe,
 };
