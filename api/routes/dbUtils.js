7b565757da0018ec79ac1b5bb1531f4fd008ec9e
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-03 16:57:58 +0100

    Update dbUtils.js

--- api/routes/dbUtils.js
+++ api/routes/dbUtils.js
@@ -1,17 +1,25 @@
 /* eslint-disable max-len */
 const express = require('express');
-const { getInfluencers } = require('../models/dbUtils');
+const { getInfluencers, getInfluencerInformation } = require('../models/dbUtils');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  console.log('je suis dans la route');
-
   const listInfluencers = await getInfluencers();
 
   if (!listInfluencers) return res.sendStatus(500);
 
   return res.json(listInfluencers);
+});
+
+router.get('/:id', async (req, res) => {
+  const influenceurInfo = getInfluencerInformation(req?.params?.id);
+
+  if (!influenceurInfo) {
+    return res.sendStatus(500);
+  }
+
+  return res.json(influenceurInfo);
 });
 
 module.exports = router;
