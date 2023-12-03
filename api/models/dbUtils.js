a0669930c06aba7ed862b623db9a9b3ecba69b4c
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-03 16:58:24 +0100

    Update dbUtils.js
    NE FONCTIONNE PAS !!!

--- api/models/dbUtils.js
+++ api/models/dbUtils.js
@@ -22,6 +22,46 @@ async function getInfluencers() {
   return listInfluencers;
 }
 
+async function getInfluencerInformation(idInfluenceur) {
+  const dico = {};
+
+  try {
+    const influenceursObject = await pb.collection('influencers').getFullList({
+      filter: `id = "${idInfluenceur}"`,
+    });
+    const influenceur = influenceursObject[0];
+    dico.influencuer = influenceur;
+
+    const listPlaforme = await pb.collection('platforms').getFullList({
+      filter: '+name',
+    });
+
+    console.log(listPlaforme);
+
+    const urls = await pb.collection('urls').getFullList({
+      filter: `influenceur = "${idInfluenceur}"`,
+    });
+
+    console.log(urls);
+
+    const platforms = {};
+    listPlaforme.forEach((platform) => {
+      urls.forEach((url) => {
+        if (url.includes(platform.toLowerCase())) {
+          platforms[platform.name] = url.url;
+        }
+      });
+    });
+    dico.platforms = platforms;
+  } catch (error) {
+    return error;
+  }
+
+  console.log(`dico = ${dico}`);
+  return dico;
+}
+
 module.exports = {
+  getInfluencerInformation,
   getInfluencers,
 };
