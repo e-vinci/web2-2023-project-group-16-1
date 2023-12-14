2d25622b1baf873213ab08ec502e83227ff90e85
GauthierC <116072358+Rhunys@users.noreply.github.com>
2023-12-14 15:47:47 +0100

    Refactor : HomePage (Search bar & feed)

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,17 @@
 const HomePage = () => {
+  const html = `
+  <div class="flex-auto">
+    <div class="px-2 py-5">
+      <input type="text" placeholder="Search" class="input input-bordered w-full max-w-xs" />
+    </div>
+
+    <div>
+      <a class="twitter-timeline" data-lang="fr" data-height="1000" data-theme="dark" href="https://twitter.com/ZeratoR?ref_src=twsrc%5Etfw">Tweets by ZeratoR</a> 
+    </div>
+  </div>`
+  
   const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  main.innerHTML = html;
 };
 
 export default HomePage;
