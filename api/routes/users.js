1c561b278b0b9764f46b7ae8480e3b06578f90d0
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-15 20:00:16 +0100

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,19 +1,20 @@
 /* eslint-disable max-len */
 const express = require('express');
-const { subscribe, unSubscribe } = require('../models/users');
+const { subscribe, unSubscribe, getSubscriptions } = require('../models/users');
 
 const router = express.Router();
 
 router.post('/subscribe', async (req, res) => {
-  const url = req?.body?.url?.length !== 0 ? req.body.url : undefined;
+  const influencer = req?.body?.influencer?.length !== 0 ? req.body.influencer : undefined;
+  const platform = req?.body?.platform?.length !== 0 ? req.body.platform : undefined;
 
-  if (!url) return res.sendStatus(400); // 400 Bad Request
+  if (!influencer || !platform) return res.sendStatus(400); // 400 Bad Request
 
-  const subscription = await subscribe(url);
+  const subscription = await subscribe(influencer, platform);
 
   if (!subscription) return res.sendStatus(500);
 
-  return res.json(subscription);
+  return res.sendStatus(200);
 });
 
 router.post('/unsubscribe', async (req, res) => {
@@ -26,6 +27,16 @@ router.post('/unsubscribe', async (req, res) => {
   if (!unsubscription) return res.sendStatus(500);
 
   return res.json(unsubscription);
+});
+
+router.get('/', async (req, res) => {
+  const subscriptions = getSubscriptions(req?.params?.id);
+
+  if (!subscriptions) {
+    return res.sendStatus(500);
+  }
+
+  return res.json(subscriptions);
 });
 
 module.exports = router;
