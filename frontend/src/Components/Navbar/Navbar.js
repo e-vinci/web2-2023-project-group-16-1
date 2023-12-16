43a5a24e485f4829240a5f2091374020ef30b90b
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-16 12:52:45 +0100

    Update Navbar.js

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line no-unused-vars
+import { getAuthenticatedUser } from "../../utils/auths"; 
+
 /**
  * Render the Navbar which is styled by using Bootstrap
  * Each item in the Navbar is tightly coupled with the Router configuration :
@@ -23,7 +24,21 @@ const Navbar = () => {
   </div>
 </div>
   `;
-  navbarWrapper.innerHTML = navbar;
+  const navbarAuth = `
+  <div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">SocialSync</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="#" data-uri="/">Home</a></li>
+      <li><a href="#" data-uri="/logout">Logout</a></li>
+      <li><a href="#" data-uri="/settings">Account</a></li>
+    </ul>
+  </div>
+</div>
+  `
+  navbarWrapper.innerHTML = getAuthenticatedUser()? navbarAuth : navbar;
 };
 
 export default Navbar;
