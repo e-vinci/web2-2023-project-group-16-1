fa75e5d4892c8f868641849b52104ccd15fc62ef
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-16 12:53:06 +0100

    Update auths.js

--- frontend/src/utils/auths.js
+++ frontend/src/utils/auths.js
@@ -2,24 +2,28 @@ let currentUser;
 
 // eslint-disable-next-line consistent-return
 const getAuthenticatedUser = () => {
-  if (currentUser !== undefined) return currentUser;
-  const serializedUser = localStorage.getItem("user");
-  if (!serializedUser) return undefined;
+  if (currentUser !== undefined) {
+    return currentUser;
+  }
+  const serializedUser = localStorage.getItem('user');
+  if (!serializedUser) {
+    return undefined;
+  }
   currentUser = JSON.parse(serializedUser);
-}
-// défini l'utilisateur authentifié actuellement 
-const setAuthenticatedUser = (authenticatedUser, ) => {
+};
+// défini l'utilisateur authentifié actuellement
+const setAuthenticatedUser = (authenticatedUser) => {
   const serializedUser = JSON.stringify(authenticatedUser);
-  localStorage.setItem("user", serializedUser);
+  localStorage.setItem('user', serializedUser);
   currentUser = authenticatedUser;
 };
 
-// indique si l'utiliseur actuel est authentifié ou pas 
+// indique si l'utiliseur actuel est authentifié ou pas
 const isAuthenticated = () => currentUser !== undefined;
 
 // retire l'utilisateur authentifié
 const clearAuthenticatedUser = () => {
-  localStorage.removeItem("user");
+  localStorage.removeItem('user');
   currentUser = undefined;
 };
 
