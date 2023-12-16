06360d6f86e0afe86dd37ae40c3a599f3b9c9ab6
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-16 17:29:57 +0100

    deploye DB postgres

--- api/utils/postgres.js
+++ api/utils/postgres.js
@@ -1,17 +1,23 @@
 const { Client } = require('pg');
 
 let client;
+const connectionString = 'postgres://lqbelykr:IKBvEmsmPVU-0vJMDJgNH4BrqXA8EWle@flora.db.elephantsql.com/lqbelykr';
 
 function postgresConnexion() {
-  client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    password: '',
-    port: 5432,
+  client = new Client(connectionString);
+
+  client.connect((err) => {
+    if (err) {
+      console.error(err);
+    }
   });
 
-  client.connect();
-
+  client.query('SELECT NOW() AS "theTime"', (err, result) => {
+    if (err) {
+      console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+  });
   return client;
 }
 
