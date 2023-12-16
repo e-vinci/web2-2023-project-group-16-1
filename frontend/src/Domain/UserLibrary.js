446c80dce0ff991d88671d0ae05fd999c4b4ac76
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-16 12:53:53 +0100

    Update UserLibrary.js

--- frontend/src/Domain/UserLibrary.js
+++ frontend/src/Domain/UserLibrary.js
@@ -1,95 +1,62 @@
-import Navbar from '../Components/Navbar/Navbar';
 import Navigate from '../Components/Router/Navigate';
+import Navbar from '../Components/Navbar/Navbar';
 import { setAuthenticatedUser } from '../utils/auths';
 
 async function login() {
   const btn = document.getElementById('loginbtn');
+
   btn.addEventListener('click', async (e) => {
-    e.preventdefault();
+    e.preventDefault();
 
-    const email = document.getElementById('emaillog').value;
-    const password = document.getElementById('pwdlog').value;
+    try {
+      const email = document.getElementById('emaillog').value;
+      const password = document.getElementById('pwdlog').value;
 
-    const newData = {
-      email,
-      password,
-    };
+      const newData = {
+        email,
+        password,
+      };
 
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(newData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(newData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
 
-    const reponse = await fetch(`/api/auths/login`, options);
-    if (!reponse.ok) {
-      throw new Error(`fetch error : ${reponse.status} : ${reponse.statusText}`);
-    } else {
-      const user = await reponse.json();
+      const response = await fetch(`/api/auths/login`, options);
 
-      // sets the Authenticated user to the actual user
-      await setAuthenticatedUser(user);
+      if (!response.ok) {
+        throw new Error(`Fetch error: ${response.status} - ${response.statusText}`);
+      }
 
-      // reloads Navbar (display is different when user logged in)
-      await Navbar();
+      const user = await response.json();
 
-      // navigte to homePage
-      // clearActive();
-      await Navigate('/');
+      setAuthenticatedUser(user);
+
+      Navbar();
+      Navigate('/');
+    } catch (err) {
+      console.error('error: ', err);
     }
   });
 }
 
 async function register() {
   const btn = document.getElementById('register');
-  // const btnLogin = document.getElementById('login');
 
-  /* btnLogin.addEventListener('click', async (e) => {
-      e.preventDefault();
-      clearActive();
-      Navigate('/login');
-    });
-    */
-
-  // Adding the user after pressing the submit button
   btn.addEventListener('click', async (e) => {
     e.preventDefault();
 
     // Recovery of all data with id
-    // const lastname = document.getElementById('nom').value;
-    // const firstname = document.getElementById('prenom').value;
     const email = document.getElementById('emailreg').value;
     const username = document.getElementById('usernamereg').value;
     const password = document.getElementById('pwdreg').value;
     const passwordConfirm = document.getElementById('pwdConfirmreg').value;
 
-    // const passwordConfirmed = document.getElementById('pwdreg2').value;
-
-    // If values are undifined
-    /*
-      if (
-        // lastname.value === undefined ||
-        // firstname.value === undefined ||
-        email.value === undefined ||
-        password.value === undefined // ||
-        // passwordConfirmed.value === undefined
-      ) {
-        // eslint-disable-next-line no-console
-        console.log("complete all fields");
-      }
-      */
-
-    /* if (password !== passwordConfirmed) {
-        console.log("passwords dont match");
-      }
-      */
-
     // Creation of a new json object
     const newData = {
-      // lastname,
-      // firstname,
       email,
       username,
       password,
@@ -98,7 +65,7 @@ async function register() {
 
     try {
       const options = {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        method: 'POST',
         body: JSON.stringify(newData),
         headers: {
           'Content-Type': 'application/json',
