c22affa0c086653a071d7ac0ce84c99409bb54ec
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-16 11:10:15 +0100

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,37 +1,42 @@
 /* eslint-disable max-len */
 const express = require('express');
 const { subscribe, unSubscribe, getSubscriptions } = require('../models/users');
+const { authorize } = require('../utils/auths');
 
 const router = express.Router();
 
-router.post('/subscribe', async (req, res) => {
+router.post('/subscribe', authorize, async (req, res) => {
+  const userId = req?.userId?.length !== 0 ? req.userId : undefined;
   const influencer = req?.body?.influencer?.length !== 0 ? req.body.influencer : undefined;
   const platform = req?.body?.platform?.length !== 0 ? req.body.platform : undefined;
 
   if (!influencer || !platform) return res.sendStatus(400); // 400 Bad Request
 
-  const subscription = await subscribe(influencer, platform);
+  const subscription = await subscribe(userId, influencer, platform);
 
   if (!subscription) return res.sendStatus(500);
 
   return res.sendStatus(200);
 });
 
-router.post('/unsubscribe', async (req, res) => {
+router.post('/unsubscribe', authorize, async (req, res) => {
+  const userId = req?.userId?.length !== 0 ? req.userId : undefined;
   const influencer = req?.body?.influencer?.length !== 0 ? req.body.influencer : undefined;
   const platform = req?.body?.platform?.length !== 0 ? req.body.platform : undefined;
 
-  if (!influencer || !platform) return res.sendStatus(400); // 400 Bad Request
+  if (!userId || !influencer || !platform) return res.sendStatus(400); // 400 Bad Request
 
-  const unsubscription = await unSubscribe(influencer, platform);
+  const unsubscription = await unSubscribe(userId, influencer, platform);
 
   if (!unsubscription) return res.sendStatus(500);
 
   return res.json(unsubscription);
 });
 
-router.get('/', async (req, res) => {
-  const subscriptions = await getSubscriptions(req?.params?.id);
+router.get('/', authorize, async (req, res) => {
+  const userId = req?.userId?.length !== 0 ? req.userId : undefined;
+
+  const subscriptions = await getSubscriptions(userId);
 
   if (!subscriptions) {
     return res.sendStatus(500);
