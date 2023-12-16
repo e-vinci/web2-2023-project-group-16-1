bf6afc97c21fc717e8812217ab83783cfa0298c1
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-16 22:34:09 +0100

    Update postgres.js

--- api/utils/postgres.js
+++ api/utils/postgres.js
@@ -1,23 +1,17 @@
 const { Client } = require('pg');
 
 let client;
-const connectionString = 'postgres://lqbelykr:IKBvEmsmPVU-0vJMDJgNH4BrqXA8EWle@flora.db.elephantsql.com/lqbelykr';
 
 function postgresConnexion() {
-  client = new Client(connectionString);
-
-  client.connect((err) => {
-    if (err) {
-      console.error(err);
-    }
+  client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    password: '',
+    port: 5432,
   });
 
-  client.query('SELECT NOW() AS "theTime"', (err, result) => {
-    if (err) {
-      console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-  });
+  client.connect();
+
   return client;
 }
 
